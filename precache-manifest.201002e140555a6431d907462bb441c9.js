d6f86e389b14c86503eff71f7ae9aafcfcfbf6c5
yandeu <yandeu@users.noreply.github.com>
2026-04-01 19:13:20 +0000

    deploy: 7353f349e3fe1cbf7fa1ed0252e777669352795e

---
+++ precache-manifest.201002e140555a6431d907462bb441c9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc370f5963017a46279a",
+    "url": "game.f601123f7955b8795f86.js"
+  },
+  {
+    "revision": "895a111464ab166c4a17",
+    "url": "vendors.78b7c76ef17afd3077ee.js"
+  },
+  {
+    "revision": "e55406a92924fcb3890ee9dbd0458e78",
+    "url": "index.html"
+  },
+  {
+    "revision": "fce01705782b2b0cfd332771d6c1fabf",
+    "url": "sw.js"
+  },
+  {
+    "revision": "57040e5677322118f6d56a1d9e43c5c6",
+    "url": "assets/icons/favicon.ico"
+  },
+  {
+    "revision": "af5c1203bb508a8ca58ad6564b62a46d",
+    "url": "assets/icons/icons-192.png"
+  },
+  {
+    "revision": "8e8fe5793eebae1ea472e35efb982b58",
+    "url": "assets/icons/icons-512.png"
+  },
+  {
+    "revision": "95add5ae695303ca3fcb52a5b1aba60d",
+    "url": "assets/img/car-body.png"
+  },
+  {
+    "revision": "05ff330f45121732338a695b67134903",
+    "url": "assets/img/car-wheel.png"
+  },
+  {
+    "revision": "f9ea4ce1455483e71ece5fdf2172e1f7",
+    "url": "assets/img/restart-btn.png"
+  },
+  {
+    "revision": "b79f5c630b31c8fccfb5d9e8c8287ddd",
+    "url": "assets/img/tile.png"
+  },
+  {
+    "revision": "735b5a83cfd0dfd278dcbf364c462c0f",
+    "url": "assets/img/wholes-small.png"
+  },
+  {
+    "revision": "ed49048255da4bdaadf5b03991e204ad",
+    "url": "assets/img/sky.png"
+  },
+  {
+    "revision": "2abdc6aa49e910d8676d90113ae91c73",
+    "url": "assets/terrain.svg"
+  },
+  {
+    "revision": "79573c486e22b34de1a769a7e5076d2f",
+    "url": "assets/img/grass.png"
+  },
+  {
+    "revision": "17a48be59e9cf44e29c0aee359fb9349",
+    "url": "assets/textureAtlas.png"
+  },
+  {
+    "revision": "87700e15b50fc50161127b5aadbfff30",
+    "url": "assets/img/gas.png"
+  },
+  {
+    "revision": "abdf954b5a1bf7a891f4bafc3797d7f7",
+    "url": "assets/textureAtlas.json"
+  },
+  {
+    "revision": "b14fdd36c61d7a9c32c0beaed4177bcc",
+    "url": "assets/textureAtlas.tps"
+  }
+];
---
+++ sw.js
@@ -0,0 +1,4 @@
+importScripts("precache-manifest.201002e140555a6431d907462bb441c9.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
